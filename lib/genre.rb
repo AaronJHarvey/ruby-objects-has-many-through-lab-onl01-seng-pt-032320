a2145da08cb66d62372837e20de14690c80e78a6
Aaron J Harvey <harvey.aaronj@gmail.com>
2020-04-27 01:58:03 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,14 +1,26 @@
 class Genre
-attr_accessor :name
-@@all = []
+  attr_accessor :name
 
-def initialize(name)
-  @name = name 
-  @@all << self
-end
+  @@all = []
 
-def self.all
-  @@all 
-end
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
 
+  def self.all
+    @@all
+  end
+
+  def songs
+    Song.all { |song| song.genre == self }
+  end
+
+  def artists
+    songs.map(&:artist)
+  end
+
+  def add_song(song)
+    songs << song
+  end
 end
