08c095c4a2a1376dd69351393e1dc0e410bdfb95
Aaron J Harvey <harvey.aaronj@gmail.com>
2020-04-27 01:52:09 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,16 @@
+class Song 
+attr_accessor :name, :artist, :genre 
+@@all = []
+
+def initialize(name, genre, artist)
+  @name = name 
+  @genre = genre 
+  @artist = artist 
+  @@all << self
+end
+
+def self.all 
+  @@all 
+end
+
+end
